4fdd8e9a7399b886b91bcad0ad04cce969e76466
simplydikka <diddeto@gmail.com>
2020-12-24 18:44:14 +0200

    updated google-services.json  and admin panel link

--- app/src/main/java/tech/studiozebra/pytniznaci/Config.java
+++ app/src/main/java/tech/studiozebra/pytniznaci/Config.java
@@ -3,7 +3,7 @@ package tech.studiozebra.pytniznaci;
 public class Config {
 
     //this is the path of your admin panel url
-    public static final String SERVER_URL = "http://studiozebra.tech/admin_panel";
+    public static final String SERVER_URL = "https://simplydikka.com/adminPanelTest";
 
     public static final boolean ENABLE_ADMOB_BANNER_ADS = true;
     public static final boolean ENABLE_ADMOB_INTERSTITIAL_ADS = true;
